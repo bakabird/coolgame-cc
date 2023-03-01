87fcc0a0f5d85a9bcacbba215d4fc6b4b120a3d5
bakabird <bakabird@sina.cn>
2023-03-01 11:17:24 +0800

    v0.0.4 增加多参数的 Action/Func 定义

--- dist/coolgame-cc.d.ts
+++ dist/coolgame-cc.d.ts
@@ -185,7 +185,16 @@ declare module 'coolgame-cc/SKPGame' {
 declare module 'coolgame-cc/Define' {
     export type Action = () => void;
     export type Action1<A> = (a: A) => void;
+    export type Action2<A, B> = (a: A, b: B) => void;
+    export type Action3<A, B, C> = (a: A, b: B, c: C) => void;
+    export type Action4<A, B, C, D> = (a: A, b: B, c: C, d: D) => void;
+    export type Action5<A, B, C, D, E> = (a: A, b: B, c: C, d: D, e: E) => void;
     export type Func<Ret> = () => Ret;
+    export type Func1<Ret, A> = (a: A) => Ret;
+    export type Func2<Ret, A, B> = (a: A, b: B) => Ret;
+    export type Func3<Ret, A, B, C> = (a: A, b: B, c: C) => Ret;
+    export type Func4<Ret, A, B, C, D> = (a: A, b: B, c: C, d: D) => Ret;
+    export type Func5<Ret, A, B, C, D, E> = (a: A, b: B, c: C, d: D, e: E) => Ret;
     export type types_constructor<T = unknown> = new (...args: any[]) => T;
     export interface IDisposable {
         Dispose(): void;
